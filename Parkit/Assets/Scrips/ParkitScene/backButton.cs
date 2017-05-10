ee47eb894f53aed48f603447277544155c350300
cmunozf <cmunozf@eafit.edu.co>
2017-05-10 15:31:20 -0500

    Correccion errores UI

---
+++ Parkit/Assets/Scrips/ParkitScene/backButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backButton : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel("MainScene");
+	}
+}
